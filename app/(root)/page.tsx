7811d7c78a870c10320351332a3d76ffe491a607
SHABI, Moshood Olawale <shabimoshood07@gmail.com>
2024-07-08 17:20:55 +0100

    Feature: authentication and authorization  using clerk and convex

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
 "use client";
-import PodcastCard from '@/components/PodcastCard'
-import { podcastData } from '@/constants';
+import PodcastCard from "@/components/PodcastCard";
+import { podcastData } from "@/constants";
+import { api } from "@/convex/_generated/api";
 // import { useQuery } from "convex/react";
 // import { api } from "@/convex/_generated/api";
 // import LoaderSpinner from '@/components/LoaderSpinner';
+import { useQuery } from "convex/react";
 
 const Home = () => {
   // const trendingPodcasts = useQuery(api.podcasts.getTrendingPodcasts);
@@ -12,7 +14,7 @@ const Home = () => {
 
   return (
     <div className="mt-9 flex flex-col gap-9 md:overflow-hidden">
-      <section className='flex flex-col gap-5'>
+      <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
 
         <div className="podcast_grid">
@@ -28,7 +30,7 @@ const Home = () => {
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://game-wren-2.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
